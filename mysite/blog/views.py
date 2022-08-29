46d65e611a13cc7c1337fc0aa008cedc8bc19877
Nasim nasiri <nasirinasim77@gmail.com>
2022-08-29 18:31:55 +0430

    25

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Article,Category
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def Home(request):
+	article_list=Article.objects.published()
+	page = request.GET.get('page',1)
+
+	paginator = Paginator(article_list,2)
+	try:
+		article = paginator.page(page)
+	except PageNotAnInteger:
+		article = paginator.page(1)
+	except EmptyPage:
+		article = paginator.page(paginator.num_pages)
+
 	context={
-	"articles":Article.objects.published(),
+	"articles":article,
 	}
 	return render(request,'blog/home.html',context)
 
