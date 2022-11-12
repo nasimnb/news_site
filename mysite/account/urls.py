e7e86f795b98dc919e61f5f5632432ed4be425f6
Nasim <nasirinasim77@gmail.com>
2022-11-12 17:51:58 +0330

    59

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -4,7 +4,8 @@ from .views import (
 	ArticleList,
 	ArticleCreate,
 	ArticleUpdate,
-	ArticleDelete
+	ArticleDelete,
+	Profile
 	)
 
 app_name="account"
@@ -39,7 +40,8 @@ urlpatterns = [
 
 urlpatterns += [
 	path('',ArticleList.as_view(),name="home"),
-	path('article/create',ArticleCreate.as_view(),name="article_create"),
+	path('article/create/',ArticleCreate.as_view(),name="article_create"),
 	path('article/update/<int:pk>',ArticleUpdate.as_view(),name="article_update"),
 	path('article/delete/<int:pk>',ArticleDelete.as_view(),name="article_delete"),
+	path('profile/',Profile.as_view(),name="profile"),
 ]
