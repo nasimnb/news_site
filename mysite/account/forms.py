021579b7ff362902609ff6f797ce5a56070dbe7b
Nasim <nasirinasim77@gmail.com>
2022-11-24 17:43:02 +0330

    69

--- mysite/account/forms.py
+++ mysite/account/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
 
 class  ProfileForm(forms.ModelForm):
 
@@ -18,3 +21,12 @@ class  ProfileForm(forms.ModelForm):
 	class Meta:
 		model=User
 		fields=['username','email','first_name','last_name','special_user','is_author']
+
+
+
+class SignupForm(UserCreationForm):
+	email = forms.EmailField(max_length=200)  
+
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password1', 'password2')
